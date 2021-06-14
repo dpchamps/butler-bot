0f0077d27bfefeeca5c9d5238cd3d2f6e4470e44
dpchamps <me@davecampion.com>
2021-06-13 21:04:05 -0700

    fix some stuff

--- src/services/listen-service/sermon.ts
+++ src/services/listen-service/sermon.ts
@@ -1,5 +1,5 @@
 import {Message} from "discord.js";
-import {getRandom, range, STATIC_DIR, tap} from "../../util";
+import {getRandom, range, STATIC_DIR} from "../../util";
 import {promises as fs} from 'fs';
 import {join} from 'path';
 import {speak} from "../../bot/speak";
@@ -10,7 +10,7 @@ const prepareSermon = (sermon: string) => fs.readdir(join(STATIC_DIR, "time-cube
     .then(x => join(STATIC_DIR, 'time-cube', x))
     .then(file => ({content: speak(`Wonderful day for some cubic thought.\n\n${sermon}`), files: [file]}));
 
-export const sermon = async (message: Message) =>
+export const sermon = (message: Message) =>
     fs.readFile(join(STATIC_DIR, "time-cube", "sermon.txt"), 'utf-8')
         .then(x => x.split("\n").filter(Boolean))
         .then(lines => range(0, 3).map(() => getRandom(lines)))
--- src/util.ts
+++ src/util.ts
@@ -1,20 +1,27 @@
 import {join, resolve} from "path";
 
 export const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
 export type Awaited<T> = T extends Promise<infer U> ? U : T;
+
 export type Option<T> = T | undefined;
 
 export const getRandom = <T>(arr: Array<T>) =>
   arr[Math.floor(Math.random() * arr.length)];
+
 export const multiline = (...args: Array<{ toString(): string }>) =>
   args.join("\n");
+
 export const unreachable = (x: never): never => {
   throw new Error("unreachable!");
 };
 export const unimplemented = () => new Error("unimplemented!");
+
 export const exists = <T>(x: Option<T>): x is T => typeof x !== "undefined";
+
 export const orDefault = <T>(x: Option<T>, fallback: T) =>
   typeof x === "undefined" ? fallback : x;
+
 export const mockingCase = (sentence: string) =>
   sentence.split("").reduce((str, char, idx) => {
     return `${str}${idx % 2 === 0 ? char : char.toUpperCase()}`;
