d48768e5e7ba147db0253ed03e1d386dc7dfa233
dpchamps <me@davecampion.com>
2021-06-19 11:33:31 -0700

    lint staged

--- src/services/listen-service/echo.test.ts
+++ src/services/listen-service/echo.test.ts
@@ -1,20 +1,20 @@
-import {echo} from "./echo";
-import {Message} from "discord.js";
-import {BotCommand} from "../../bot/parseCommand";
-import {speak} from "../../bot/speak";
+import { echo } from "./echo";
+import { Message } from "discord.js";
+import { BotCommand } from "../../bot/parseCommand";
+import { speak } from "../../bot/speak";
 
 describe("echo", () => {
-    it("Should echo things", () => {
-        const fakeMessage = {
-            channel: {
-                send: jest.fn()
-            }
-        } as unknown as Message;
+  it("Should echo things", () => {
+    const fakeMessage = {
+      channel: {
+        send: jest.fn(),
+      },
+    } as unknown as Message;
 
-        echo(fakeMessage, {content: ["hello", "world"]} as BotCommand);
+    echo(fakeMessage, { content: ["hello", "world"] } as BotCommand);
 
-        expect(fakeMessage.channel.send).toHaveBeenCalledWith(
-            expect.stringMatching("I will now repeat what you asked: \"hello. world\"")
-        )
-    })
+    expect(fakeMessage.channel.send).toHaveBeenCalledWith(
+      expect.stringMatching('I will now repeat what you asked: "hello. world"')
+    );
+  });
 });
