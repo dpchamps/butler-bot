340568a045e7d12062e4d62f11aafb298d35de1e
dpchamps <me@davecampion.com>
2021-06-23 23:42:35 -0700

    store container first

--- cd/deploy-newest.sh
+++ cd/deploy-newest.sh
@@ -1,5 +1,6 @@
 set -euxo pipefail
 
+CONTAINER=$(docker ps -a | awk -v image=$IMAGE '$2==image { print $1 }')
 docker pull $IMAGE
-docker stop $(docker ps -a | awk -v image=$IMAGE '$2==image { print $1 }')
+docker stop $CONTAINER
 docker run -e DEBUG=0 --env-file .production.env -d --rm $IMAGE
