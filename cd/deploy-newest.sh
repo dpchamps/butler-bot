c506231f474685838943db9103c8cc10f0d31d40
dpchamps <me@davecampion.com>
2021-06-23 23:32:07 -0700

    tweaks

--- cd/deploy-newest.sh
+++ cd/deploy-newest.sh
@@ -1,5 +1,4 @@
 set -euxo pipefail
 
-docker ps -a
-docker kill $(docker ps -a | awk '$2=="$IMAGE" { print $1 }')
+docker stop $(docker ps -a | awk -v image=$IMAGE '$2==image { print $1 }')
 docker run -e DEBUG=0 --env-file .production.env -d --rm $IMAGE
