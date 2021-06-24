05f524e70c12828a9181bfce6e962208cdab7cbb
dpchamps <me@davecampion.com>
2021-06-23 23:10:05 -0700

    set env var

--- cd/deploy-newest.sh
+++ cd/deploy-newest.sh
@@ -1,5 +1,5 @@
 set -euxo pipefail
 
 docker ps -a
-docker kill $(docker ps -a | awk '$2=="${{ secrets.DOCKERHUB_USERNAME }}/butler-bot:latest" { print $1 }')
-docker run -e DEBUG=0 --env-file .production.env -d --rm ${{ secrets.DOCKERHUB_USERNAME }}/butler-bot:latest
+docker kill $(docker ps -a | awk '$2=="$IMAGE" { print $1 }')
+docker run -e DEBUG=0 --env-file .production.env -d --rm $IMAGE
