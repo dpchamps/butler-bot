aa542da3f98bab940bc5f6fca7ba099ba7faff00
dpchamps <me@davecampion.com>
2021-06-27 15:52:56 -0700

    add emoji table

--- db/create-tables.sql
+++ db/create-tables.sql
@@ -1,4 +1,9 @@
 CREATE TABLE IF NOT EXISTS memories (
-    id      SERIAL PRIMARY KEY
-    author_id
+    author_id VARCHAR(255) UNIQUE NOT NULL
+    content TEXT
 )
+
+CREATE TABLE IF NOT EXISTS emojis {
+    emoji_id VARCHAR(255) UNIQUE NOT NULL
+    used INT
+}
