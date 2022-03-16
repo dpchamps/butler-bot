908e428c46e8bafc7c467c4cc674bd94078ee3df
dpchamps <me@davecampion.com>
2022-03-15 18:33:32 -0700

    order up

--- src/services/listen-service/order-up.ts
+++ src/services/listen-service/order-up.ts
@@ -1,11 +1,11 @@
 import { Message } from "discord.js";
 import { BotCommand } from "../../bot/parseCommand";
-import { speak } from "../../bot/speak";
-import { orDefault, textIf } from "../../util";
+import { textIf } from "../../util";
 import { getAuthorFromReference } from "../../discord-client/message-utils";
 
-const nothingBurger = "<nothing_burger:950100007913136169>";
+const nothingBurger = `<:nothing_burger:950100007913136169>`;
 const bell = ":bellhop:";
+const waiter = ":person_tipping_hand:";
 
 const getAuthorFromMessage = (message: Message) =>
   message.reference?.messageID
@@ -16,9 +16,16 @@ const getAuthorFromMessage = (message: Message) =>
     : Promise.resolve();
 
 export const orderUp = (message: Message, _c: BotCommand) =>
-  getAuthorFromMessage(message).then(
-    (author) =>
-      `${author}${textIf(author, ": ")}${[bell, bell, bell, nothingBurger].join(
-        " "
-      )}`
-  );
+  getAuthorFromMessage(message)
+    .then(
+      (author) =>
+        `${textIf(author, `${author}: `)}${[
+          bell,
+          bell,
+          bell,
+          "\n",
+          waiter,
+          nothingBurger,
+        ].join(" ")}`
+    )
+    .then((x) => message.channel.send(x));
