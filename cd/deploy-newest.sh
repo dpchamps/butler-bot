57d417ffc771b9e948324c198fe0d9591a87e494
David Campion <gasolinewaltz@gmail.com>
2022-06-17 17:51:52 -0700

    Update deploy-newest.sh
    Conditionally stop container

--- cd/deploy-newest.sh
+++ cd/deploy-newest.sh
@@ -2,5 +2,7 @@ set -euxo pipefail
 
 CONTAINER=$(docker ps -a | awk -v image=$IMAGE '$2==image { print $1 }')
 docker pull $IMAGE
-docker stop $CONTAINER
+
+if [ -n "$CONTAINER" ]; then docker stop $CONTAINER; fi
+
 docker run -e DEBUG=0 --env-file .production.env -d --rm $IMAGE
